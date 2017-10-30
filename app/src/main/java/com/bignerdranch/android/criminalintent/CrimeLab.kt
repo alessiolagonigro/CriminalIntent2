61af797f6ac2c967f9fd8190958325a9b3006f5d
alessio.lagonigro <alessio.lagonigro@gmail.com>
2017-10-30 18:00:36 +0100

    Commit test

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.kt
@@ -0,0 +1,44 @@
+package com.bignerdranch.android.criminalintent
+
+import android.content.Context
+
+import java.util.ArrayList
+import java.util.UUID
+
+/**
+ * Created by alessio.lagonigro on 30/10/2017.
+ */
+
+class CrimeLab(context: Context) {
+    val crimes: List<Crime>
+
+    init {
+        crimes = ArrayList()
+        for(i in 0..100-1){
+            var crime = Crime()
+            crime.title = "Crime #$i"
+            crime.solved=i%2==0
+            crimes.add(crime)
+        }
+
+    }
+
+    fun getCrime(id: UUID): Crime? {
+        return crimes.firstOrNull { it.id == id }
+    }
+
+    companion object {
+        private var sCrimeLab: CrimeLab? = null
+
+        operator fun get(context: Context): CrimeLab {
+
+            sCrimeLab = when (sCrimeLab) {
+                null -> CrimeLab(context)
+                else -> sCrimeLab
+            }
+            return sCrimeLab as CrimeLab
+        }
+    }
+
+
+}
